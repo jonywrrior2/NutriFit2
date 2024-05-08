48531af9db5f0d1bcceb97fb4d736792f375b1f9
jonywrrior2 <cristianbersabe@gmail.com>
2024-05-08 14:34:03 +0200

    Peso txt y botones de subir y bajar peso puestos

--- app/src/main/java/com/example/nutrifit/activities/DatosBioActivity.kt
+++ app/src/main/java/com/example/nutrifit/activities/DatosBioActivity.kt
@@ -3,8 +3,10 @@ package com.example.nutrifit.activities
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.nutrifit.R
+import com.example.nutrifit.dbUser.DatabaseManagerUser
 
 class DatosBioActivity : AppCompatActivity() {
 
@@ -13,6 +15,7 @@ class DatosBioActivity : AppCompatActivity() {
         setContentView(R.layout.activity_datos_bio)
 
         val volverButton: Button = findViewById(R.id.volverPerfilActivityB)
+        val txtPesoUser: TextView = findViewById(R.id.txtPesoUserB)
 
         volverButton.setOnClickListener {
             intent = Intent(this@DatosBioActivity, PerfilActivity::class.java)
@@ -21,5 +24,19 @@ class DatosBioActivity : AppCompatActivity() {
         }
 
         val currentUserEmail = intent.getStringExtra("email")
+
+        currentUserEmail?.let { email ->
+            DatabaseManagerUser.getUserByEmail(email,
+                onSuccess = { user ->
+                    user?.let {
+                        txtPesoUser.text = it.peso.toString()
+
+                    }
+                },
+                onFailure = { exception ->
+
+                }
+            )
+        }
     }
 }
