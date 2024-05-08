a38578bf4deac00b0b67a9bb2aa9fdd55b87709c
jonywrrior2 <cristianbersabe@gmail.com>
2024-05-08 14:13:50 +0200

    Decimal en el pes y empezando layout de datos bio

--- app/src/main/java/com/example/nutrifit/activities/DatosBioActivity.kt
+++ app/src/main/java/com/example/nutrifit/activities/DatosBioActivity.kt
@@ -1,6 +1,8 @@
 package com.example.nutrifit.activities
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.example.nutrifit.R
 
@@ -9,5 +11,15 @@ class DatosBioActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_datos_bio)
+
+        val volverButton: Button = findViewById(R.id.volverPerfilActivityB)
+
+        volverButton.setOnClickListener {
+            intent = Intent(this@DatosBioActivity, PerfilActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+        val currentUserEmail = intent.getStringExtra("email")
     }
 }
