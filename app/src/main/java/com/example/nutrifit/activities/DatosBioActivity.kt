2f107a466d3b3880281212ed92e7ba830504649a
jonywrrior2 <cristianbersabe@gmail.com>
2024-05-10 11:28:35 +0200

    Limites a los metodos de subir y bajar datos de peso, altura y edad puestos

--- app/src/main/java/com/example/nutrifit/activities/DatosBioActivity.kt
+++ app/src/main/java/com/example/nutrifit/activities/DatosBioActivity.kt
@@ -18,6 +18,7 @@ class DatosBioActivity : AppCompatActivity() {
         val volverButton: Button = findViewById(R.id.volverPerfilActivityB)
         val txtPesoUser: TextView = findViewById(R.id.txtPesoUserB)
         val txtAlturaUser: TextView = findViewById(R.id.txtAlturaUserB)
+        val txtEdad : TextView = findViewById(R.id.txtEdadUserB)
 
         volverButton.setOnClickListener {
             intent = Intent(this@DatosBioActivity, PerfilActivity::class.java)
@@ -33,6 +34,7 @@ class DatosBioActivity : AppCompatActivity() {
                     user?.let {
                         txtPesoUser.text = it.peso.toString()
                         txtAlturaUser.text = it.altura.toString()
+                        txtEdad.text = it.edad.toString()
 
 
                     }
@@ -48,7 +50,7 @@ class DatosBioActivity : AppCompatActivity() {
     fun restarPeso(view: View) {
         val txtPesoUser: TextView = findViewById(R.id.txtPesoUserB)
         var pesoActual = txtPesoUser.text.toString().toDoubleOrNull() ?: 0.0
-        if (pesoActual > 0.0) {
+        if (pesoActual > 20.0) {
             pesoActual -= 0.1
             txtPesoUser.text = String.format("%.1f", pesoActual)
         }
@@ -57,15 +59,17 @@ class DatosBioActivity : AppCompatActivity() {
     fun sumarPeso(view: View) {
         val txtPesoUser: TextView = findViewById(R.id.txtPesoUserB)
         var pesoActual = txtPesoUser.text.toString().toDoubleOrNull() ?: 0.0
-        pesoActual += 0.1
-        txtPesoUser.text = String.format("%.1f", pesoActual)
+        if (pesoActual < 200.0) {
+            pesoActual += 0.1
+            txtPesoUser.text = String.format("%.1f", pesoActual)
+        }
     }
 
 
     fun restarAltura(view: View) {
         val txtAlturaUser: TextView = findViewById(R.id.txtAlturaUserB)
         var alturaActual = txtAlturaUser.text.toString().toIntOrNull() ?: 0
-        if (alturaActual > 0) {
+        if (alturaActual > 100) {
             alturaActual -= 1
             txtAlturaUser.text = alturaActual.toString()
         }
@@ -74,12 +78,29 @@ class DatosBioActivity : AppCompatActivity() {
     fun sumarAltura(view: View) {
         val txtAlturaUser: TextView = findViewById(R.id.txtAlturaUserB)
         var alturaActual = txtAlturaUser.text.toString().toIntOrNull() ?: 0
-        if (alturaActual == 0) {
-            alturaActual = 1
-        } else {
+        if (alturaActual < 250) {
+
             alturaActual += 1
-        }
         txtAlturaUser.text = alturaActual.toString()
+        }
+    }
+
+    fun restarEdad(view: View) {
+        val txtEdad: TextView = findViewById(R.id.txtEdadUserB)
+        var edadActual = txtEdad.text.toString().toIntOrNull() ?: 0
+        if (edadActual > 10) {
+            edadActual -= 1
+            txtEdad.text = edadActual.toString()
+        }
+    }
+
+    fun sumarEdad(view: View) {
+        val txtEdad: TextView = findViewById(R.id.txtEdadUserB)
+        var edadActual = txtEdad.text.toString().toIntOrNull() ?: 0
+        if (edadActual < 100) {
+            edadActual += 1
+        txtEdad.text = edadActual.toString()
+        }
     }
 
 
