af136d058599fa9a0d6edef3d96d8cf603ab8a62
jonywrrior2 <cristianbersabe@gmail.com>
2024-05-20 09:39:10 +0200

    Cambiado metodo de filtrar alimentos para que haya to lower case y filtre aunque se escriba todo mayúscula, minúscula, etc

--- app/src/main/java/com/example/nutrifit/databases/DatabaseManager.kt
+++ app/src/main/java/com/example/nutrifit/databases/DatabaseManager.kt
@@ -9,21 +9,21 @@
         private val alimentosCollection = db.collection("alimentos")
         fun buscarAlimentos(palabraClave: String, callback: (List<Alimento>) -> Unit) {
             val resultados = mutableListOf<Alimento>()
+            val palabraClaveLower = palabraClave.toLowerCase()
 
             alimentosCollection
-                .whereGreaterThanOrEqualTo("nombre", palabraClave)
-                .whereLessThanOrEqualTo("nombre", palabraClave + "\uf8ff")
                 .get()
                 .addOnSuccessListener { documents ->
                     for (document in documents) {
-                        val nombre = document.getString("nombre")
-                        val calorias = document.getDouble("kcal") ?: 0.0
-                        val proteinas = document.getDouble("proteinas")?: 0.0
-                        val cantidad = document.getLong("cantidad")?.toDouble() ?: 0.0
-                        val unidad = document.getString("unidad")
+                        val nombre = document.getString("nombre") ?: ""
+                        if (nombre.toLowerCase().contains(palabraClaveLower)) {
+                            val calorias = document.getDouble("kcal") ?: 0.0
+                            val proteinas = document.getDouble("proteinas") ?: 0.0
+                            val cantidad = document.getLong("cantidad")?.toDouble() ?: 0.0
+                            val unidad = document.getString("unidad")
                             val alimento = Alimento(nombre, calorias, proteinas, cantidad, unidad)
                             resultados.add(alimento)
-
+                        }
                     }
                     callback(resultados)
                 }
